092c36f86cddb49d7ef630a9ed838e0e1d4c8127
emdiple <emdiple.95@gmail.com>
2025-03-23 23:56:10 +0330

    template of UI messages updated (multi-message supported)

--- frontend/components/AlertMsg.vue
+++ frontend/components/AlertMsg.vue
@@ -1,12 +1,23 @@
 <template>
-    <div
-        v-if="indexStore.showAlert"
-        role="alert"
-        class="alert fixed top-20 right-3 z-40 w-auto min-w-64 max-w-[90%] md:max-w-[50%] xl:max-w-[40%]"
-        :class="`alert-${indexStore.alertVariant}`"
-    >
-        <SvgIcon :name="indexStore.alertVariant" />
-        {{ indexStore.alertMsg }}
+    <div class="alert-container fixed top-20 right-3 z-40 flex flex-col space-y-3 items-end w-full">
+        <div
+            v-if="indexStore.showAlert"
+            role="alert"
+            class="alert w-auto min-w-64 max-w-[90%] md:max-w-[50%] xl:max-w-[40%]"
+            :class="`alert-${indexStore.alertVariant}`"
+        >
+            <SvgIcon :name="indexStore.alertVariant" />
+            {{ indexStore.alertMsg }}
+        </div>
+        <div
+            v-if="indexStore.secShowAlert"
+            role="alert"
+            class="alert w-auto min-w-64 max-w-[90%] md:max-w-[50%] xl:max-w-[40%]"
+            :class="`alert-${indexStore.secAlertVariant}`"
+        >
+            <SvgIcon :name="indexStore.secAlertVariant" />
+            {{ indexStore.secAlertMsg }}
+        </div>
     </div>
 </template>
 
