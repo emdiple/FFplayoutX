3b2c65015477bcaeba954ec5f74927dd6b8f9c76
emdiple <emdiple.95@gmail.com>
2025-03-25 14:58:12 +0330

    Fixed migration failure

--- migrations/0005_add_advendor_endpoint.sql
+++ migrations/0005_add_advendor_endpoint.sql
@@ -1,3 +1,3 @@
 -- Add migration script here
-ALTER TABLE global ADD COLUMN advendor_endpoint TEXT NOT NULL DEFAULT 'http://localhost:8000';
+ALTER TABLE global ADD COLUMN advendor_endpoint TEXT NOT NULL DEFAULT 'http://localhost:8008';
 ALTER TABLE channels ADD COLUMN advendor_endpoint TEXT NOT NULL DEFAULT '';
